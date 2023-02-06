bbb69adda6ffa963027b261550f37a43a35f46ed
Łukasz Szewczyk <lukicall@o2.pl>
2023-02-06 16:27:33 +0100

    dzien 4

---
+++ ChallengeApp/ChallengeApp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿var name = "Ewa";
+var sex = "Kobieta";
+var age = 33;
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == "Mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+
+
+
+
